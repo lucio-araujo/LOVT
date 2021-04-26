f9a3b68664132d7e88d4f3aa1a5ed1bc47218d8c
lucio-araujo <lucio.ribeiroaraujo@gmail.com>
2021-04-26 14:24:07 -0300

    Modal de newsletter

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const nightMode = document.querySelector('#night-mode');
 nightMode.addEventListener('click', () => {
   document.documentElement.classList.toggle('night-mode');
 });
+
+var modal = document.getElementById("myModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+setTimeout(function() {
+  modal.style.display = "block"}, 5000);
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
