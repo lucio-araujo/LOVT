6bc01fa3692742fc16bf05246988adde68541f23
lucio-araujo <lucio.ribeiroaraujo@gmail.com>
2021-04-27 10:07:44 -0300

    Melhorias no código

--- js/main.js
+++ js/main.js
@@ -9,16 +9,19 @@ var modal = document.getElementById("myModal");
 
 var span = document.getElementsByClassName("close")[0];
 
-setTimeout(function() {
-  modal.style.display = "block"}, 5000);
+function timeModal() {
+  setTimeout(function() {
+    modal.style.display = "block"}, 5000);
+}
 
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+timeModal();
