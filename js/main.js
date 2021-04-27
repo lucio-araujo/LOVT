3e3488870e7733f8e060a084847938069c98737a
lucio-araujo <lucio.ribeiroaraujo@gmail.com>
2021-04-27 14:12:23 -0300

    Validação de formulário com JS

--- js/main.js
+++ js/main.js
@@ -25,3 +25,44 @@ window.onclick = function(event) {
 }
 
 timeModal();
+
+// document.getElementById('btn-submit-contact').addEventListener("click", (evento) => {
+//   evento.preventDefault();
+
+//   let nomeValue = document.querySelector("#input_nome").value;
+//   let emailValue = document.querySelector("#input_email").value;
+
+//   console.log(nomeValue);
+//   console.log(emailValue);
+
+// });
+
+let formContact = querySelector("#form_contact");
+
+formContact.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  let nome = document.querySelector("#input_nome").value;
+
+  let email = document.querySelector("#input_email").value;
+
+  let telefone = document.querySelector("#input_telefone").value;
+
+  let texto = document.querySelector("#mensagem").value;
+
+  if (!nome || !email || !telefone || !texto) {
+    alert("Prencha todos os campos!")
+  }
+  
+  if (nome.length < 3) {
+    alert("Seu nome deve ter dois ou mais caracteres")
+  }
+  
+  if (telefone.length < 8) {
+    alert("Seu telefone deve ter no minimo 8 digitos")
+  }
+  
+  if (email.indexOf('@') < 0 || email.indexOf('.') < 0) {
+    alert("Campo de email invalido!")
+  }
+});
